cc953b31a2d7f37f8aefcc3c7f6d4b4e42f8e903
Dani666rc <85699882+Dani666rc@users.noreply.github.com>
2023-10-27 00:52:40 -0500

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import seaborn as sns
-
+import tkinter as TK 
+from turtle import width
 
 # CARGAR LOS DATOS, CREAR Y LIMPIAR DATAFRAME BASE
 def data():
